f326f8809d10aba79200f0b0703d1f9a6ec57bcd
Richard Lack <rilack@microsoft.com>
2025-03-22 16:12:43 -0700

    attempted frontend import fixes

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
+import App from './App';
 
 const container = document.getElementById('root');
 if (!container) throw new Error("Could not find root element");
