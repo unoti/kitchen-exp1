2d4a5fd4291661f4df35162e1f0795d204b99720
Richard Lack <rilack@microsoft.com>
2025-03-22 16:48:55 -0700

    saving everything before vite

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import LandingPage from './components/LandingPage';
+import LandingPage from './components/LandingPage.tsx'; // I shouldn't need the .tsx extension, but things are malfunctioning. Troubleshooting.
 import { FluentProvider, webLightTheme } from '@fluentui/react-components';
 
 const App: React.FC = () => {
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './App.tsx'; // Why in the name of all that is holy does this not work without the .tsx extension?
 
 const container = document.getElementById('root');
 if (!container) throw new Error("Could not find root element");
